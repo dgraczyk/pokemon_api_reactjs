487b625c1c11400309d1e82045faf7ecae48837d
dgraczyk <dtgraczyk@gmail.com>
2021-03-16 15:47:38 +0100

    Added NavBarContainer

---
+++ src/components/NavBarContainer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+
+function NavBarContainer () {
+  return (
+    <AppBar position="relative">
+        <Toolbar>
+          Navbar    
+        </Toolbar>
+    </AppBar>
+  )
+}
+
+export default NavBarContainer
