5f81de747b4523b4323e00e813014036e7189b25
dgraczyk <dtgraczyk@gmail.com>
2021-03-16 21:07:29 +0100

    Creating pokemon_list

---
+++ src/components/pokemon_list/PokemonItem.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 250    
+  },
+  img: {
+    objectFit: "fill"
+  }
+});
+
+const PokemonItem = props => {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia 
+          className={classes.img}
+          component="img"
+          height="150"
+          alt={props.pokemon.name}
+          image={props.pokemon.img}
+          title={props.pokemon.name}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2" text="center">
+            {props.pokemon.name}
+          </Typography>          
+        </CardContent>
+      </CardActionArea>      
+    </Card>
+  )
+}
+
+PokemonItem.propTypes = {
+  pokemon: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    img: PropTypes.string.isRequired,
+    types: PropTypes.arrayOf(PropTypes.shape({
+      name: PropTypes.string.isRequired
+    })).isRequired
+  }).isRequired
+}
+
+export default PokemonItem
--- src/components/pokemon_list/PokemonListContainer.js
+++ src/components/pokemon_list/PokemonListContainer.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import PokemonItem from './PokemonItem'
 
 const PokemonListContainer = (props) => {
+  const pokemon = {
+    id: 1,
+    name: "bulbasaur",
+    img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg",
+    types: [
+      {name: "grass"},
+      {name: "poison"},
+    ]
+  }
+  
   return (
     <div>
-      PokemonListContainer
+      <PokemonItem pokemon={pokemon}/>
     </div>
   )
 }
